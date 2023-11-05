e7f180097c03f7613a503be6515f502914a92422
Holliday Sims <72518147+hesims@users.noreply.github.com>
2023-11-05 07:52:13 -0500

    push

--- workspace/src/pages/Files.jsx
+++ workspace/src/pages/Files.jsx
@@ -13,6 +13,9 @@ function Files() {
     <div>
       <div className='file-username'>@username</div>
       <img src={AddMedia} className='add-media'/>
+      <img src={AddFolder} className='add-folder'/>
+      <img src={Trash} className='trash'/>
+      <img src={Logout} className='logout'/>
       <svg xmlns="http://www.w3.org/2000/svg" width="1337" height="666" viewBox="0 0 1337 666" fill="none" className='transparent'>
         <path d="M0 25C0 11.1929 11.1929 0 25 0H1312C1325.81 0 1337 11.1929 1337 25V641C1337 654.807 1325.81 666 1312 666H25C11.1929 666 0 654.807 0 641V25Z" fill="#D9D9D9" fill-opacity="0.23"/>
       </svg>
