da2c25e0512b7da57eb5c71cc169044cd800c7a5
sabrina <jsabrina949@gmail.com>
2023-11-05 01:34:44 -0400

    front enc ui is fine

---
+++ workspace/src/App.js
@@ -0,0 +1,21 @@
+import './App.css'; 
+
+function App() { 
+  return ( 
+  <div className="colored-background">
+  <head> <link rel="stylesheet" href="https://fonts.cdnfonts.com/css/cowboys" /> 
+  </head> 
+  <body> 
+  <h1 className="main-title">Outlaw Network</h1> 
+  {/* Button container with space between buttons */} 
+  <div className="button-container"> 
+  <button className="buttons" type="submit">Register</button> 
+  <button className="buttons" type="submit">Login</button> 
+  </div> 
+  </body> 
+  </div> 
+  ); 
+} 
+
+
+  export default App;
