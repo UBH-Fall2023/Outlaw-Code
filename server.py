854f0065ef4f38233126900857d2fcf490a38689
CPompey1 <cpompey10@gmail.com>
2023-11-05 00:19:29 -0400

    image uploads work

--- server.py
+++ server.py
@@ -1,9 +1,10 @@
-from flask import Flask, make_response, send_from_directory
-
+from flask import Flask, make_response, send_from_directory,request
+import os
+from werkzeug.utils import secure_filename
 app = Flask(__name__)
 
 
-@app.route("/<path:path>")
+@app.route("/<path:path>",methods=['GET','POST'])
 def getPage(path):
     print(path)
     root = '.'
@@ -22,3 +23,18 @@ def index():
     response.headers['X-Content-Type-Options'] = 'nosniff'
 
     return response
+
+# @app.route('/upload_file.html',methods=['POST'])
+# def upload()
+@app.route('/upload_file',methods=['GET','POST'])
+def upload_file():
+    response = 'ya modda'
+    if request.method == 'POST':
+        if 'file' in request.files:
+            file = request.files['file']
+            file.save(f'{os.getcwd()}/{secure_filename(file.filename)}')
+    return response
+    
+
+if __name__ == '__main__':
+    app.run(port=5001)
