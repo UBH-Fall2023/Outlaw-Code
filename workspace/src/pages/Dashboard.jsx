de81aca7c27f97726d564f847b9058c6517f1cdc
sabrina <jsabrina949@gmail.com>
2023-11-05 09:19:43 -0500

    page links not working

--- workspace/src/pages/Dashboard.jsx
+++ workspace/src/pages/Dashboard.jsx
@@ -1,13 +1,34 @@
+import './Dashboard.css';
+import { Link } from 'react-router-dom';
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import React from 'react';
 
-// A functional component for the dashboard page
 function Dashboard() {
   return (
-    <div>
-      <h1>Welcome to Your Dashboard</h1>
-
-      
+    <Router>
+    <div className="colored-background">
+      <head>
+        <link rel="stylesheet" href="https://fonts.cdnfonts.com/css/cowboys" />
+      </head>
+      <body>
+        <h1 className="main-title">Outlaw Network</h1>
+        {/* Button container with space between buttons */}
+        <div className="button-container">
+          <Link to="/register">
+            <button className="buttons" type="submit">
+              Register
+            </button>
+          </Link>
+          <Link to="/login">
+            <button className="buttons" type="submit">
+              Login
+            </button>
+          </Link>
+        </div>
+      </body>
     </div>
+    </Router>
   );
 }
 
--- workspace/src/pages/Register.jsx
+++ workspace/src/pages/Register.jsx
@@ -1,13 +1,60 @@
-import React from 'react';
+import React, {useState} from 'react';
+import './Register.css';
 
-// A functional component for the dashboard page
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+
 function Register() {
-  return (
-    <div>
-      <h1>Register!</h1>
 
-      
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Login form submitted with data:', formData);
+   };
+  const [formData, setFormData] = useState({ username: '', password: '' });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  return (
+    <Router>
+    <div className="Reg">
+      <header className="Reg-header">
+      <link
+        rel="stylesheet"
+        href="https://fonts.cdnfonts.com/css/cowboys"
+      />
+        
+      </header>
+      <body>
+      <button className="Header-title">Outlaw Network</button>
+          <div className="Reg-rectangle">
+            <div className="enterforms">
+              <label htmlFor="username" className='username'>Username:</label>
+                <input className='input-box'
+                type="username"
+                id="username"
+                name="username"
+                value={formData.username}
+                onChange={handleInputChange}
+                required
+                /> 
+              <label htmlFor="password" className='password'>Password:</label>
+                <input className='input-box'
+                type="password"
+                id="password"
+                name="password"
+                value={formData.password}
+                onChange={handleInputChange}
+                required
+                /> 
+            </div>
+          </div>
+          <button className="Reg-button"> Register </button>
+      </body>
     </div>
+    </Router>
   );
 }
 
